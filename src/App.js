d2124d5f265454ab57aa559a95619ec1245aef60
Sandeep Kumar Patel <125953981+skpatel-bmj@users.noreply.github.com>
2023-03-31 17:00:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <div>BMJ LOGO</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
